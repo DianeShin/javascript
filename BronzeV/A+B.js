a8038c4b7b07bad6e84f504f8b5190f963d8bb0f
DianeShin <jadore845@snu.ac.kr>
2023-06-25 13:45:44 +0200

    upload A+B.js

---
+++ BronzeV/A+B.js
@@ -0,0 +1,35 @@
+/* 
+Question link : https://leetcode.com/problems/two-sum/
+
+Runtime : 152 ms
+Memory : 9480 KB
+
+Time Complexity : O(1)
+
+Space Complexity : O(1)
+
+Algorithm :
+1. Create interface
+2. Take input from user
+3. parseInt, add, return
+
+Learned :
+1. How to input/output from console
+*/
+
+// prep
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// take input
+rl.question('', AB => {
+    // parse input
+    let [A, B] = AB.split(" ");
+    // print output
+    console.log(parseInt(A)+parseInt(B));
+    // close
+    rl.close();
+});
